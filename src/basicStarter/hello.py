aaab43c581a466fb7b83b2418c32cb9049f3b7e0
dusaiya <dusaiya@126.com>
2017-08-13 01:42:21 +0800

    修改页面

--- src/basicStarter/hello.py
+++ src/basicStarter/hello.py
@@ -1,18 +1,25 @@
 # encoding: utf-8
+
 from flask import Flask
 app = Flask(__name__)
 from flask import render_template
 
-from neo4j.v1 import GraphDatabase,basic_auth
-##必须按照如下写，即使是在 10.200.6.5 这台机器上
-driver = GraphDatabase.driver("bolt://10.200.6.5:7687", auth=basic_auth("neo4j", "ictsoftware"))
-neo_session = driver.session()
+from py2neo import Graph, Node, Relationship
+neo_graph = Graph(
+    "http://10.200.6.5:7474/db/data/", 
+    username="neo4jTest", 
+    password="ictsoftware"
+)
 
 @app.route('/search')
-@app.route('/search/<name>')
-def search(name=None):
-    print name
-    return render_template('search.html', name=name)
+@app.route('/search/<nick_name>')
+def search(nick_name=None):
+    print nick_name
+    ob=neo_graph.find_one("Weibo", "nick_name", nick_name)
+    newDictOb=dict(ob)
+    labels=ob.labels()._SetView__items #获取label集合
+    return render_template('search.html', name=newDictOb,labels=labels)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
