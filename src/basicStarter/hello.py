8b796d69c20a3db7dcf0dcdccc6a293ab18887f4
dusaiya <dusaiya@126.com>
2017-08-16 09:05:33 +0800

    初步功能实现

--- src/basicStarter/hello.py
+++ src/basicStarter/hello.py
@@ -15,10 +15,28 @@ neo_graph = Graph(
 @app.route('/search/<nick_name>')
 def search(nick_name=None):
     print nick_name
-    ob=neo_graph.find_one("Weibo", "nick_name", nick_name)
-    newDictOb=dict(ob)
-    labels=ob.labels()._SetView__items #获取label集合
-    return render_template('search.html', name=newDictOb,labels=labels)
+    nodelist=[]
+    myrel=None
+    ob=neo_graph.find_one("Douban", "name", nick_name)
+    print ob
+    nodelist.append(ob)
+    for rel in neo_graph.match(start_node=ob):
+        nodelist.append(rel.end_node())
+        myrel=rel
+        break
+    newDictList=[]
+    for nodeNeo in nodelist:
+        newDictOb=dict(nodeNeo)
+        labelsets=nodeNeo.labels()._SetView__items
+        for  nodelabel in labelsets:
+            newDictOb["label"]= nodelabel.encode("utf-8")
+        newDictList.append(newDictOb)
+    
+    startNode=newDictList[0]
+    endNode=newDictList[1]
+    
+    return render_template('search.html', nodeDataList=newDictList,
+                           startNode=startNode,endNode=endNode)
 
 if __name__ == '__main__':
     app.debug = True
