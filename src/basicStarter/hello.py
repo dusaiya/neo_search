ed778a41feb1a856ea39d8894562cb36ff702c61
dusaiya <dusaiya@126.com>
2017-08-21 15:33:53 +0800

    增加root的User节点

--- src/basicStarter/hello.py
+++ src/basicStarter/hello.py
@@ -17,12 +17,15 @@ def neoob2dict(ob):
         将ob对象转换成dict对象
     '''
     newDictOb = dict(ob)
-    labelsets = ob.labels()._SetView__items
-    for nodelabel in labelsets:
-        newDictOb["label"]= nodelabel.encode("utf-8")
-        break ##只能有一个标签
+    newDictOb["label"]= get_neo_label(ob)
     return newDictOb
 
+def get_neo_label(ob):
+    labelsets = ob.labels()._SetView__items
+    for nodelabel in labelsets:
+        labelstr= nodelabel.encode("utf-8")
+        break
+    return labelstr
 
 def dict2json(newDictOb):
     '''
@@ -31,11 +34,15 @@ def dict2json(newDictOb):
     if newDictOb["label"] == "Douban":
         jsonNode = json.dumps({"id":newDictOb["neo_id"], 
                 "name":newDictOb["name"], 
-                "size":40, "category":0})
+                "size":40, "category":1})
     elif newDictOb["label"] == "Weibo":
         jsonNode = json.dumps({"id":newDictOb["neo_id"], 
                 "name":newDictOb["nick_name"], 
-                "size":40, "category":1})
+                "size":40, "category":2})
+    elif newDictOb["label"] == "User":
+        jsonNode = json.dumps({"id":newDictOb["neo_id"], 
+                "name":newDictOb["neo_id"], 
+                "size":80, "category":0})
     return jsonNode
 
 @app.route('/search')
@@ -45,23 +52,28 @@ def search(nick_name=None):
     newDictList=[]
     jsonNodeList=[]
     ob=neo_graph.find_one("Douban", "name", nick_name)
-    sourceDictOb=neoob2dict(ob)
+    rootOb=None
+    for rel1 in neo_graph.match(end_node=ob):
+        if get_neo_label(rel1.start_node())=='User':
+            rootOb=rel1.start_node()
+    
+    sourceDictOb=neoob2dict(rootOb)
     newDictList.append(sourceDictOb)
     jsonNodeList.append(dict2json(sourceDictOb))
     ##根据豆瓣作为source节点进行查找
-    for rel in neo_graph.match(start_node=ob):
-        targetDictOb=neoob2dict(rel.end_node())
+    leafNodeList = neo_graph.match(start_node=rootOb)
+    for rel1 in leafNodeList:
+        targetDictOb=neoob2dict(rel1.end_node())
         newDictList.append(targetDictOb)
         jsonNodeList.append(dict2json(targetDictOb))
-        relLabel=rel._Relationship__type.__str__()
+        relLabel=rel1._Relationship__type.__str__()
         myrel=json.dumps({"source":sourceDictOb["neo_id"],  "target":targetDictOb["neo_id"],
                           "label":{"normal":{"show":True,"formatter":relLabel}} })
         rellist.append(myrel)
-    
-#     for rel in neo_graph.match(end_node=ob):
-#         targetDictOb=neoob2dict(rel.start_node())
-#         newDictList.append(targetDictOb)
-#         jsonNodeList.append(dict2json(targetDictOb))
+    ##再将每个子节点
+#     for rel2 in leafNodeList:
+#         targetDictOb=neoob2dict(rel2.endNode())
+#         if neo_graph.match(start_node=targetDictOb):
 #         myrel=json.dumps({"source":targetDictOb["neo_id"],  "target":sourceDictOb["neo_id"]  })
 #         rellist.append(myrel)
     
